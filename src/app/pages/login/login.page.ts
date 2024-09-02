aed1b668a8cf504b50275ac02055b4f2b4952608
wulan <wulanidh168@gmail.com>
2024-09-03 01:02:25 +0700

    pengaturan login page

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -6,7 +6,7 @@ import { AppState } from 'src/store/AppState';
 import { Store } from "@ngrx/store";
 import { hide, show } from 'src/store/loading/loading.actions';
 import { login,  recoverPassword } from 'src/store/login/login.actions';
-import { ToastController } from '@ionic/angular';
+import { NavController, ToastController } from '@ionic/angular';
 import { LoginState } from 'src/store/login/LoginState';
 import { Subscription } from 'rxjs';
 
@@ -21,7 +21,7 @@ export class LoginPage implements OnInit, OnDestroy {
   loginStateSubscription!: Subscription;
 
   constructor(private router: Router, private formBuilder: FormBuilder, private store: Store<AppState>,
-    private toastController: ToastController) { }
+    private toastController: ToastController, private navController: NavController) { }
 
   ngOnInit() {
     this.form = new LoginPageForm(this.formBuilder).createForm();
@@ -52,7 +52,7 @@ export class LoginPage implements OnInit, OnDestroy {
 
   private onIsLoggedIn(loginState: LoginState) {
     if (loginState.isLoggedIn) {
-      this.router.navigate(['home']);
+      this.navController.navigateRoot(['home']);
     }
   }
 
